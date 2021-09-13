cd23692099d4eb47a6dd80941ecf6b1f501224f4
Yann Hulot <yhulot78410@gmail.com>
2021-09-13 22:16:18 +1000

    replaced fastify with Hapi

--- prisma/migrations/20210913112948_init/migration.sql
+++ prisma/migrations/20210913112948_init/migration.sql
@@ -1,5 +1,5 @@
 -- CreateEnum
-CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
+CREATE TYPE "TokenType" AS ENUM ('EMAIL', 'API');
 
 -- CreateTable
 CREATE TABLE "User" (
@@ -8,9 +8,23 @@ CREATE TABLE "User" (
     "email" TEXT NOT NULL,
     "firstName" TEXT NOT NULL,
     "lastName" TEXT NOT NULL,
-    "role" "Role" NOT NULL DEFAULT E'USER',
+    "isAdmin" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Token" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "type" "TokenType" NOT NULL,
+    "emailToken" TEXT,
+    "valid" BOOLEAN NOT NULL DEFAULT true,
+    "expiration" TIMESTAMP(3) NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Token_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -22,7 +36,7 @@ CREATE TABLE "Job" (
     "endDate" TIMESTAMP(3),
     "companyName" VARCHAR(255) NOT NULL,
     "title" VARCHAR(255) NOT NULL,
-    "description" VARCHAR(2000) NOT NULL,
+    "description" TEXT NOT NULL,
     "userId" INTEGER,
     "stack" TEXT[],
 
@@ -31,6 +45,12 @@ CREATE TABLE "Job" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Token_emailToken_key" ON "Token"("emailToken");
+
+-- AddForeignKey
+ALTER TABLE "Token" ADD CONSTRAINT "Token_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Job" ADD CONSTRAINT "Job_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
---
+++ src/server.ts
@@ -0,0 +1,46 @@
+import Hapi from '@hapi/hapi';
+import hapiAuthJWT from 'hapi-auth-jwt2';
+import prismaPlugin from './plugins/prisma';
+import emailPlugin from './plugins/email';
+import usersPlugin from './plugins/users';
+import statusPlugin from './plugins/status';
+import authPlugin from './plugins/auth';
+import dotenv from 'dotenv';
+import hapiPino from 'hapi-pino';
+
+dotenv.config();
+
+const server: Hapi.Server = Hapi.server({
+  port: process.env.PORT || 3000,
+  host: process.env.HOST || '0.0.0.0',
+});
+
+export async function createServer(): Promise<Hapi.Server> {
+  // Register the logger
+  await server.register({
+    plugin: hapiPino,
+    options: {
+      logEvents: process.env.CI === 'true' || process.env.TEST === 'true' ? false : undefined,
+      prettyPrint: process.env.NODE_ENV !== 'production',
+      // Redact Authorization headers, see https://getpino.io/#/docs/redaction
+      redact: ['req.headers.authorization'],
+    },
+  });
+
+  await server.register([hapiAuthJWT, authPlugin, prismaPlugin, emailPlugin, statusPlugin, usersPlugin]);
+
+  await server.initialize();
+
+  return server;
+}
+
+export async function startServer(server: Hapi.Server): Promise<Hapi.Server> {
+  await server.start();
+  server.log('info', `Server running on ${server.info.uri}`);
+  return server;
+}
+
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
