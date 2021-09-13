186561dcbfdcc01a4531a73340f1bfad06b41f48
Yann Hulot <yhulot78410@gmail.com>
2021-09-13 22:31:45 +1000

    add more tests

---
+++ tests/helpers/test-helpers.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from '@prisma/client';
+import { TokenType } from '@prisma/client';
+import { add } from 'date-fns';
+import { AuthCredentials } from '@hapi/hapi';
+
+// Helper function to create a test user and return the credentials object the same way that the auth plugin does
+export const createUserCredentials = async (prisma: PrismaClient, isAdmin: boolean): Promise<AuthCredentials> => {
+  const testUser = await prisma.user.create({
+    data: {
+      email: `test-${Date.now()}@test.com`,
+      firstName: `test-${Date.now()}`,
+      lastName: `test-${Date.now()}`,
+      isAdmin,
+      tokens: {
+        create: {
+          expiration: add(new Date(), { days: 7 }),
+          type: TokenType.API,
+        },
+      },
+    },
+    include: {
+      tokens: true,
+    },
+  });
+
+  return {
+    userId: testUser.id,
+    tokenId: testUser.tokens[0].id,
+    isAdmin: testUser.isAdmin,
+  };
+};
