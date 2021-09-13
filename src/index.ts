3f26cbd1b3e997200ea7a6dc77d4447f1f321e8c
Yann Hulot <yhulot78410@gmail.com>
2021-09-13 16:30:13 +1000

    add user create route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { PrismaClient } from '@prisma/client';
 import Fastify, { FastifyInstance } from 'fastify';
 import jwtVerifier from './jwtVerifier';
+import { IncomingNewUser } from './types/incoming';
 
 const prisma = new PrismaClient();
-
 const server: FastifyInstance = Fastify({ logger: true });
 
 server.route({
@@ -15,6 +15,32 @@ server.route({
   handler: async () => {
     const users = await prisma.user.findMany();
     return { success: true, payload: users };
+  },
+});
+
+server.route<{ Body: IncomingNewUser }>({
+  method: 'POST',
+  url: '/new',
+  schema: {
+    body: {
+      email: { type: 'string' },
+      firstName: { type: 'string' },
+      lastName: { type: 'string' },
+    },
+  },
+  preHandler: async (request, reply) => {
+    return jwtVerifier(request, reply);
+  },
+  handler: async (request) => {
+    const { firstName, lastName, email } = request.body;
+    const user = await prisma.user.create({
+      data: {
+        firstName: firstName,
+        lastName,
+        email,
+      },
+    });
+    return { success: true, payload: user };
   },
 });
 
---
+++ src/types/incoming.ts
@@ -0,0 +1,5 @@
+export interface IncomingNewUser {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
