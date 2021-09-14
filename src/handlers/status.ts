7bcf4be5aea228482c7441e40c22e6b81ffd25d6
Yann Hulot <yhulot78410@gmail.com>
2021-09-14 10:00:37 +1000

    fix broken status test

--- src/handlers/status.ts
+++ src/handlers/status.ts
@@ -1,5 +1,5 @@
 import { Request, ResponseToolkit } from '@hapi/hapi';
 
 export const statusHandler = (_request: Request, h: ResponseToolkit) => {
-  h.response({ up: true }).code(200);
+  return h.response({ up: true }).code(200);
 };
